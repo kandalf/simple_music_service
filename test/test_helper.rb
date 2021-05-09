b19b1f26703f05b4ff8bfb7e7ed7a92b909daf96
Leonardo Mateo <leonardomateo@gmail.com>
2021-05-09 18:39:31 -0300

    Add spawners fixtures replacement

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ require "minitest/spec"
 require "minitest/autorun"
 require "minitest/assertions"
 
+require_relative "./spawners"
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
