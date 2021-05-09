d3355b48779581d88ad72d3394bde3887271ccdf
Leonardo Mateo <leonardomateo@gmail.com>
2021-05-09 18:38:08 -0300

    Add convenience methods to models for associations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ApplicationRecord
   has_one :artist_balance
+  has_and_belongs_to_many :songs
+
+  def add_song(song)
+    self.songs << song
+  end
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -18,4 +18,30 @@ describe Album do
     assert_equal ["is not a number"], album.errors[:total_duration]
     assert_equal ["is not a number"], album.errors[:year]
   end
+
+  it "should update total duration" do
+    album = Album.spawn(name: "Core", cached_songs: {}, total_duration: 100)
+    artist = Artist.spawn(name: "Stone Temple Pilots", biography: "Fantastic rock band")
+    song = Song.spawn(name: "Plush", duration: 314, genre: :alternative_rock)
+
+    artist.songs << song
+
+    album.add_song(song)
+
+    assert_equal 414, album.total_duration
+  end
+
+  it "should cache song" do
+    album = Album.spawn(name: "Core", cached_songs: {}, total_duration: 100)
+    artist = Artist.spawn(name: "Stone Temple Pilots", biography: "Fantastic rock band")
+    song = Song.spawn(name: "Creep", duration: 333, genre: :alternative_rock)
+
+    artist.songs << song
+
+    album.add_song(song)
+
+    cached_song = { "name" => song.name, "duration" => song.duration, "genre" => song.genre, "artists" => song.artists.map(&:name) }
+
+    assert_equal cached_song, album.cached_songs[song.id.to_s]
+  end
 end
