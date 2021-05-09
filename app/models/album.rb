9e2130a10e67d425d3714dde0da621618b76f372
Leonardo Mateo <leonardomateo@gmail.com>
2021-05-09 18:38:08 -0300

    Add convenience methods to models for associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,22 @@ class Album < ApplicationRecord
   validates :name, presence: true
   validates :total_duration, numericality: true
   validates :year, presence: true, numericality: true
+
+  def add_song(song)
+    self.cached_songs ||= {}
+
+    songs = self.cached_songs.tap do |cached|
+      cached[song.id] = {
+        name: song.name,
+        duration: song.duration,
+        genre: song.genre,
+        artists: song.artists.map(&:name)
+      }
+    end
+
+    self.update(
+      total_duration: self.total_duration + song.duration,
+      cached_songs: songs
+    )
+  end
 end
